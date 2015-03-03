bc4520aa586b042b9f63ae48c7eb414093fd190c
Jeff Keen <jeff@keen.me>
2015-03-03 17:33:50 -0600

    Update for new php buildpack

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>  
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Should I Reply To All?</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <meta property="og:title" content="Should I Reply To All?" />
+  <meta property="og:url" content="http://shouldireplytoall.com" />
+  <meta property="og:image" content="http://shouldireplytoall.com/images/for-sharing.jpg"/>
+  <meta property="og:description" content="A question to consider when replying to emails.">
+  <meta name="description" content="A question to consider when replying to emails.">
+
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <script src="js/libs/modernizr-1.6.min.js"></script>
+</head>
+
+<body>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1390011921314243',
+        xfbml      : true,
+        version    : 'v2.2'
+      });
+    };
+
+    (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+  </script>
+  <div id="container">
+    <header>
+      <h1><a href="javascript:void(0);">Should I Reply To All?</a><span>(Probably not)</span></h1>
+    </header>
+    
+    <div id="main">
+      <div class="panel question first">
+        <div class="inner">
+          <h2>Should I Reply To All?</h2>
+          <p class="click-hint"><span>Click Anywhere To Continue</span></p>
+        </div>
+      </div>
+      
+      <div class="panel answer">
+        <div class="inner">
+          <h2>No.</h2>
+        </div>
+      </div>
+      
+      <div class="panel fire photo">
+        <div class="inner">
+          <img src="images/fire.jpg"/>
+          
+          <p class="caption">Unless the building is on fire.</p>
+          <p class="attribution"><a href="http://www.flickr.com/photos/bjohansson/2614591735/" title="Building on Fire! by Björn J, on Flickr">Building on Fire! by Björn J, on Flickr</a></p>
+        </div>
+      </div>
+      
+      <div class="panel attack photo">
+        <div class="inner">
+          <img src="images/bombs.jpg"/>
+          <p class="caption">Or we're currently under attack.</p>
+        </div>
+      </div>
+      
+      <div class="panel reasons third">
+        <div class="inner">
+          <p>Otherwise, please, no.</p>
+          <p>Does <em>everyone</em> need to know what you're about to say?</p>
+          <p>Actually, does <em>anyone</em> need to know what you're about to say? Because maybe you don't need to reply to that email at all.</p>
+
+        </div>
+      </div>
+
+      <div class="panel made-by fourth reasons">
+        <div class="inner">   
+          <p>Made by <a href="http://jeffkeen.me">Jeff Keen</a>, who once got so annoyed by someone replying to all that he made this helpful website.</p>
+
+          <br/>
+          <br/>
+          <div class="social-buttons">
+            <ul>
+              <li class="twitter">         
+               <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://shouldireplytoall.com" data-text="Priority: High, Re: Re: Re: Re: Re: by @realjeffkeen" data-related="realjeffkeen" data-hashtags="email" data-dnt="true">Tweet</a>
+               <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
+               <li class="facebook"><div class="fb-like" data-href="http://shouldireplytoall.com" data-layout="button_count" data-action="like" data-share="true" data-width="450" data-color-scheme="dark" data-show-faces="false"></li>
+             </ul>
+           </div>
+       </div>
+        <br/>
+        <br/>
+
+
+
+      </div>
+
+    </div>
+  </div> <!-- end of #container -->
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+  <script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+  
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+
+  <script>
+   var _gaq = [['_setAccount', 'UA-719104-6'], ['_trackPageview']];
+   (function(d, t) {
+    var g = d.createElement(t),
+    s = d.getElementsByTagName(t)[0];
+    g.async = true;
+    g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+  })(document, 'script');
+</script>
+
+</body>
+</html>
